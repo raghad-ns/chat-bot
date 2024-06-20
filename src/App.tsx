c0afd1aae1d89f5509a459633a2dacceaf1b8782
Momen-G-Ar <cp.momen@gmail.com>
2024-06-20 20:58:23 +0300

    chat box

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import React from "react";
 import "./App.css";
-import { Header } from "./components";
+import { ChatBox } from "./components";
+
+interface IChat {
+    sender: "bot" | "user";
+    text: string;
+}
 
 const App = () => {
+    const test = () => {};
+    const [text, setText] = React.useState("");
+    const [chat, setChat] = React.useState<IChat[]>([]);
+    const handleEnter = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setText("");
+        setChat([
+            ...chat,
+            { text: text, sender: "user" },
+            { text: "Your message accepted", sender: "bot" },
+        ]);
+        console.log(chat);
+    };
     return (
-        <div>
-            <Header />
+        <div className="flex h-[100vh] w-[100vw]">
+            <ChatBox
+                chat={chat}
+                text={text}
+                setText={setText}
+                onMenuClick={test}
+                onNewChatClick={test}
+                onSettingsClick={test}
+                onHelpClick={test}
+                isEmptyChat={false}
+                handleSubmit={handleEnter}
+            />
         </div>
     );
 };
