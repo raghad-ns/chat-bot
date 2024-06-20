c4bd4361e222b3c8c336765e1e9e23c44f5e0952
raghad-ns <raghadnadersalem20022017@gmail.com>
2024-06-20 20:34:05 +0300

    side bar initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import "./App.css";
-import { Header } from "./components";
 
-const App = () => {
-    return (
-        <div>
-            <Header />
+import './App.css'
+import { SideBar } from './components'
+
+function App() {
+
+  return (
+    <>
+      <div className='grid grid-cols-5 max-h-full hide-scrollbar'>
+        <SideBar />
+        <div className='col-span-4'>
+
         </div>
-    );
-};
+      </div>
+    </>
+  )
+}
 
-export default App;
+export default App
